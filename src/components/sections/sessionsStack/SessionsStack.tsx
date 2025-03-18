97eb5a68f12413e39b3296e52f7680705c3416b8
SeniduRavihara <senidumee16@gmail.com>
2025-03-18 17:36:33 +0530

    fix errors and build success

--- src/components/sections/sessionsStack/SessionsStack.tsx
+++ src/components/sections/sessionsStack/SessionsStack.tsx
@@ -11,51 +11,51 @@ const SessionsStack = ({ direction = "vertical" }) => {
   const wrapper = useRef<HTMLDivElement | null>(null);
   const section = useRef<HTMLDivElement | null>(null);
 
-   useLayoutEffect(() => {
-     if (!wrapper.current || !section.current) return;
+  useLayoutEffect(() => {
+    if (!wrapper.current || !section.current) return;
 
-     let ctx = gsap.context(() => {
-       const items = wrapper.current.querySelectorAll(".item");
-       if (!items.length) return;
+    const ctx = gsap.context(() => {
+      if (!wrapper.current || !section.current) return;
+      const items = wrapper.current.querySelectorAll(".item");
+      if (!items.length) return;
 
-       items.forEach((item, index) => {
-         if (index !== 0) {
-           gsap.set(
-             item,
-             direction === "horizontal" ? { xPercent: 100 } : { yPercent: 100 }
-           );
-         }
-       });
+      items.forEach((item, index) => {
+        if (index !== 0) {
+          gsap.set(
+            item,
+            direction === "horizontal" ? { xPercent: 100 } : { yPercent: 100 }
+          );
+        }
+      });
 
-       const timeline = gsap.timeline({
-         scrollTrigger: {
-           trigger: section.current,
-           pin: true,
-           start: "top top",
-           end: () => `+=${items.length * 100}%`,
-           scrub: 1,
-           invalidateOnRefresh: true,
+      const timeline = gsap.timeline({
+        scrollTrigger: {
+          trigger: section.current,
+          pin: true,
+          start: "top top",
+          end: () => `+=${items.length * 100}%`,
+          scrub: 1,
+          invalidateOnRefresh: true,
           //  markers: true, // Debugging markers
-         },
-         defaults: { ease: "none" },
-       });
+        },
+        defaults: { ease: "none" },
+      });
 
-       items.forEach((item, index) => {
-         timeline.to(item, { scale: 0.8, borderRadius: "10px" });
+      items.forEach((item, index) => {
+        timeline.to(item, { scale: 0.8, borderRadius: "10px" });
 
-         if (items[index + 1]) {
-           timeline.to(
-             items[index + 1],
-             direction === "horizontal" ? { xPercent: 0 } : { yPercent: 0 },
-             "<"
-           );
-         }
-       });
-     }, wrapper);
+        if (items[index + 1]) {
+          timeline.to(
+            items[index + 1],
+            direction === "horizontal" ? { xPercent: 0 } : { yPercent: 0 },
+            "<"
+          );
+        }
+      });
+    }, wrapper);
 
-     return () => ctx.revert(); // Cleanup GSAP animations on unmount
-   }, [direction]);
-
+    return () => ctx.revert(); // Cleanup GSAP animations on unmount
+  }, [direction]);
 
   return (
     <div>
@@ -80,7 +80,7 @@ const SessionsStack = ({ direction = "vertical" }) => {
                 </div>
                 <video
                   src="https://videos.pexels.com/video-files/4763824/4763824-uhd_2560_1440_24fps.mp4"
-                  loading="lazy"
+                  // loading="lazy"
                   autoPlay
                   muted
                   loop
@@ -91,15 +91,15 @@ const SessionsStack = ({ direction = "vertical" }) => {
               <div role="listitem" className="item">
                 <div className="item_content">
                   <h2 className="item_number">2</h2>
-                  <h2>The Changing Seasons: Nature’s Everlasting Cycle</h2>
+                  <h2>The Changing Seasons: Nature&apos;s Everlasting Cycle</h2>
                   <p className="item_p">
-                    Experience the beauty of nature's transitions, from blooming
-                    spring flowers to snowy winter landscapes.
+                    Experience the beauty of nature&apos;s transitions, from
+                    blooming spring flowers to snowy winter landscapes.
                   </p>
                 </div>
                 <video
                   src="https://videos.pexels.com/video-files/3214448/3214448-uhd_2560_1440_25fps.mp4"
-                  loading="lazy"
+                  // loading="lazy"
                   autoPlay
                   muted
                   loop
@@ -110,7 +110,9 @@ const SessionsStack = ({ direction = "vertical" }) => {
               <div role="listitem" className="item">
                 <div className="item_content">
                   <h2 className="item_number">3</h2>
-                  <h2>Guardians of Nature: Protecting Our Planet’s Future</h2>
+                  <h2>
+                    Guardians of Nature: Protecting Our Planet&apos;s Future
+                  </h2>
                   <p className="item_p">
                     Learn about the importance of conservation and how we can
                     work together to preserve the beauty of nature for
@@ -119,7 +121,7 @@ const SessionsStack = ({ direction = "vertical" }) => {
                 </div>
                 <video
                   src="https://videos.pexels.com/video-files/4328514/4328514-uhd_2560_1440_30fps.mp4"
-                  loading="lazy"
+                  // loading="lazy"
                   autoPlay
                   muted
                   loop
@@ -139,7 +141,7 @@ const SessionsStack = ({ direction = "vertical" }) => {
                 </div>
                 <video
                   src="https://videos.pexels.com/video-files/2871916/2871916-hd_1920_1080_30fps.mp4"
-                  loading="lazy"
+                  // loading="lazy"
                   autoPlay
                   muted
                   loop
