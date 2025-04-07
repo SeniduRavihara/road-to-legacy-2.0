c043d3a83f2177583829955390414ed3c5c437ae
SeniduRavihara <senidumee16@gmail.com>
2025-04-07 23:45:39 +0530

    invitation download is working and ready for sending emails and setup firebase clud functions

--- src/components/GenerateTicket.tsx
+++ src/components/GenerateTicket.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { invitation } from "@/assets";
+import html2canvas from "html2canvas";
 import ExportedImage from "next-image-export-optimizer";
 import { QRCodeCanvas } from "qrcode.react";
-import { useState } from "react";
+import { Button } from "./ui/button";
 
 export default function GenerateTicket({
   name = "sen",
@@ -13,47 +14,35 @@ export default function GenerateTicket({
   email: string;
   uni: string;
 }) {
-  const [ticket, setTicket] = useState("senu");
+  const handleDownload = async () => {
+    const element = document.getElementById("ticket");
+    if (!element) return;
 
-  const downloadQR = () => {
-    // const canvas = document.getElementById("myqr") as HTMLCanvasElement;
-    // const pngUrl = canvas
-    //   .toDataURL("image/png")
-    //   .replace("image/png", "image/octet-stream");
-    // const downloadLink = document.createElement("a");
-    // downloadLink.href = pngUrl;
-    // downloadLink.download = "myqr.png";
-    // document.body.appendChild(downloadLink);
-    // downloadLink.click();
-    // document.body.removeChild(downloadLink);
+    // Wait for fonts/images
+    await document.fonts.ready;
+
+    const canvas = await html2canvas(element, {
+      useCORS: true, // required if image is external
+      scale: 2, // higher quality
+    });
+
+    const imgData = canvas.toDataURL("image/png");
+
+    const link = document.createElement("a");
+    link.href = imgData;
+    link.download = "invitation.png";
+    link.click();
   };
-
-  // const generateTicket = async () => {
-  //   const ticketData = {
-  //     name: "John Doe",
-  //     event: "University Fest",
-  //     date: "2025-05-20",
-  //     seat: "A1",
-  //     qrCode: Math.random().toString(36).substring(2, 15),
-  //   };
-
-  //   const docRef = await addDoc(collection(db, "tickets"), ticketData);
-  //   setTicket({ id: docRef.id, ...ticketData });
-  // };
 
   return (
     <div>
-      {/* <button onClick={generateTicket} className="bg-blue-500 text-white">
-        Generate Ticket
-      </button> */}
-
-      <div className="relative" id="ticket">
+      <div className="relative rounded-lg" id="ticket">
         <ExportedImage
           src={invitation}
           alt="Invitation"
-          className="rounded-lg shadow-md mb-4"
           width={400}
           height={400}
+          unoptimized={true}
         />
 
         <div className="absolute top-[230px] left-[100px]">
@@ -75,9 +64,12 @@ export default function GenerateTicket({
         <h2 className="absolute top-[594px] left-[100px]">{uni}</h2>
       </div>
 
-      <button onClick={downloadQR} className="bg-green-500 text-white">
+      <Button
+        onClick={handleDownload}
+        className="bg-green-500 text-white mt-10"
+      >
         Download QR Code
-      </button>
+      </Button>
     </div>
   );
 }
