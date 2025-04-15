bf0606e4833b5f2a0bc5e5411f002e8e7d9850a9
SeniduRavihara <senidumee16@gmail.com>
2025-04-15 13:27:31 +0530

    update invitation png

--- src/components/flip-count/FlipCounter.tsx
+++ src/components/flip-count/FlipCounter.tsx
@@ -1,16 +1,25 @@
-"use client"
+"use client";
 
+import { useEffect, useState } from "react";
 import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown";
-import './FlipCount.css'
+import "./FlipCount.css";
 
 const FlipCounter = () => {
+  const [targetTime, setTargetTime] = useState<number | null>(null);
+
+  useEffect(() => {
+    // Set target time to May 31, 2025 at 00:00:00
+    const targetDate = new Date("2025-05-31T00:00:00");
+    setTargetTime(targetDate.getTime());
+  }, []);
+
+  if (!targetTime) return null; // or a loading spinner
+
   return (
-    <div className="senu">
-      <FlipClockCountdown
-        className="flip-clock relative "
-        to={new Date().getTime() + 24 * 3600 * 1000 + 5000}
-      />
+    <div className="relative top-5">
+      <FlipClockCountdown className="flip-clock relative" to={targetTime} />
     </div>
   );
-}
-export default FlipCounter
+};
+
+export default FlipCounter;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import "./Navbar.css"
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
+
+  const router = useRouter();
+
   return (
-    <div id="floating-navbar" className="bg-[#1f2227b9] p-2 w-[90vw] sm:w-[70vw] md:w-[50vw] h-14 backdrop-blur-md border rounded-xl">
+    <div
+      id="floating-navbar"
+      className="bg-[#1f2227b9] p-2 w-[90vw] sm:w-[70vw] md:w-[50vw] h-14 backdrop-blur-md border rounded-xl"
+    >
       <ul className="flex justify-around items-center h-full">
         <li
           className="cursor-pointer hover:text-gray-300 px-4 py-2 rounded-lg transition duration-300 ease-in-out bg-gradient-to-tr from-red-500 to-[#191B1F] via-[#191B1F]  hover:from-red-400 hover:to-neutral-700"
-          //   onClick={() => scrollToSection("registered")}
+          onClick={() => router.push("/register")}
         >
           Register
         </li>
